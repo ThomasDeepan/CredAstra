850461412bb72ad5d73938cbe1c2f0889cacd4e3
ThomasDeepan <thomasdeepan05@gmail.com>
2026-03-13 23:34:58 +0530

    rpc url added

--- frontend/src/web3/config.js
+++ frontend/src/web3/config.js
@@ -2,6 +2,7 @@ import { createConfig, http } from 'wagmi'
 import { QueryClient } from '@tanstack/react-query'
 import { mainnet, sepolia } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
+http("https://eth-sepolia.g.alchemy.com/v2/LC4B3y6H7tGgqrZhP6GKd")
 
 // Chains you want CredAstra to support.
 // You can change these to the networks where your contracts are deployed.
